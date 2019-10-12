4791d561b51d3abf6b94547b55a736b2378447cd
Berend Ozceri <berend@recogni.com>
2019-10-12 14:06:55 -0700

    Inhibit emitting any context if not in Perl mode.

--- vpp.pl
+++ vpp.pl
@@ -163,7 +163,8 @@ use Vpp;
 use Cwd qw(abs_path);
 
 sub EmitContext {
-  sprintf("\n# line %d %s\n",$line_number+1,$current_file) .
+  !$perl_mode ? "" :
+    sprintf("\n# line %d %s\n",$line_number+1,$current_file) .
     sprintf("print qq\001`line %d \"%s\" 0\n\001;",$line_number+1,$current_file);
 }
 
